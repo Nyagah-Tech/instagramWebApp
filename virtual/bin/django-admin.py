3e6f57e3b28b7cedc397c43d7fe1b876e220d21a
dan <dan@localhost.localdomain>
2020-01-06 04:10:58 +0300

    fix bug in the runtime.txt

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dan/Documents/moringa-school-project/Django/p1/virtual/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
